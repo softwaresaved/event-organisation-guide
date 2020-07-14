69f01f1807479c78d0fe0150cea22769ceaff9f3
Shoaib Sufi <shoaibsufi@gmail.com>
2020-07-14 13:14:48 +0100

    keeping it as version 1

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019-2020, University of Manchester, University of Edinburgh'
 author = 'Shoaib Sufi, Selina Aragon, Aleksandra Nenadic'
 
 # The short X.Y version
-version = '2.0'
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '2.0.0 beta 1'
+release = '1.1.1 beta 1'
 
 
 # -- General configuration ---------------------------------------------------
